e401c1d4b54e9d9304e5f198af304015f9fea360
Eric <rovx15@gmail.com>
2016-10-31 19:00:05 +0100

    Todo hecho, comentado, falta  traducir

--- app/src/main/java/com/example/alumnedam/practica1/Principal/MuerteActivity.java
+++ app/src/main/java/com/example/alumnedam/practica1/Principal/MuerteActivity.java
@@ -10,7 +10,8 @@ import com.example.alumnedam.practica1.R;
 import com.example.alumnedam.practica1.Utilitys.CalcularMuerte;
 
 public class MuerteActivity extends AppCompatActivity {
-
+    //Declaramos como global frase para poder tener la frase en
+    // todo momento tambien por si salta un reinicio de eventos
     private String frase;
 
 
@@ -18,24 +19,33 @@ public class MuerteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muerte);
+        //Metemos en variables nombre, edad, y vicios
         String nombre = getIntent().getStringExtra("nombre");
         int edad = getIntent().getIntExtra("edad", -33);
         boolean cbDrogas = getIntent().getBooleanExtra("cbDrogas", false);
         boolean cbAlcohol = getIntent().getBooleanExtra("cbAlcohol", false);
         boolean cbMujHom = getIntent().getBooleanExtra("cbMujHom", false);
+        //Declaramos y llamamos a CalcularMuerte para obtener un resultado
         CalcularMuerte cm = new CalcularMuerte();
         int edadMuerte = cm.calculaMuerte(edad, cbDrogas, cbAlcohol, cbMujHom);
+        //Declaramos el TextView
         TextView tvTexto = (TextView) findViewById(R.id.tvMuerte);
+        //Declaramos un array de strings
         String[] mensajesMuerte;
+        //El array de strings le decimos que contendra las frases del .xml muerte
         mensajesMuerte = getResources().getStringArray(R.array.muerte);
+        //Hacemos un Random con la longitud maxima del array
         int num = (int) Math.floor(Math.random() * (mensajesMuerte.length - 0) + 0);
+        //Declaramos frase como string con el nombre, el mensaje que saldra con el random, los años con los que muere
         frase = (nombre + " " + mensajesMuerte[num] + " " + edadMuerte + " años");
+        //En caso de haber rotado la pantalla, o otro evento que reinicie la activity, se recurre al guardado que teniamos.
         if (savedInstanceState != null) {
             frase = savedInstanceState.getString("frase");
         }
+        //Se aplica la frase a el textView
         tvTexto.setText(frase);
     }
-
+    //Metodo para guardar como esta la app en caso de rotacion u otro evento que haga un reinicio de este
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putString("frase", frase);
--- app/src/main/java/com/example/alumnedam/practica1/Utilitys/CalcularMuerte.java
+++ app/src/main/java/com/example/alumnedam/practica1/Utilitys/CalcularMuerte.java
@@ -12,18 +12,32 @@ import java.util.Random;
 
 public class CalcularMuerte {
 
-
+    /**
+     * Le llegan diferentes parametros
+     * @param Edad la edad de la persona
+     * @param droga si consume drogas
+     * @param alco si consume alcohol
+     * @param mujhom si es propenso a tener relaciones con hombres/mujeres
+     * @return y devuelve una edad
+     */
     public int calculaMuerte(int Edad, boolean droga, boolean alco, boolean mujhom) {
+        //Inicializamos el random
         Random rnd = new Random();
+        //Inicializamos la edad
         int edad;
+        //Hacemos un do while, para que mientras la edad no sea mayor a la edad que ya tiene el usuario que no pare
         do {
+            //Calculamos la edad de su muerte con un numero aleatorio
             edad = (rnd.nextInt((100 - Edad) + Edad));
+            //Si es adicto le restamos 10 años
             if (droga) {
                 edad = edad - 10;
             }
+            //Si es alcoholico le restamos 5 años
             if (alco) {
                 edad = edad - 5;
             }
+            //Si es propenso a relaciones le restamos 2 años
             if (mujhom) {
                 edad = edad - 2;
             }
